14d3983284e71d1f3bc507cf183ed78e948482f1
mcelhinneyk <mr.mcelhinney@gmail.com>
2021-12-05 12:40:19 -0800

    initial Head.js code

---
+++ src/Header.js
@@ -0,0 +1,91 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { Breadcrumbs, Link } from '@mui/material';
+import { useTheme } from '@material-ui/core/styles';
+
+function Header() {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
+
+    const handleMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        ChemPractice
+                    </Typography>
+                    <div>
+                        {isMobile ? (
+                            <>
+                                <IconButton
+                                    size="large"
+                                    edge="start"
+                                    color="inherit"
+                                    aria-label="menu"
+                                    sx={{ mr: 2 }}
+                                    onClick={handleMenu}
+                                >
+                                    <MenuIcon />
+                                </IconButton>
+                                <Menu
+                                    id="main-Menu"
+                                    anchorEl={anchorEl}
+                                    anchorOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    keepMounted
+                                    transformOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    open={Boolean(anchorEl)}
+                                    onClose={handleClose}
+                                >
+                                    <MenuItem onClick={handleClose}>Practice Selection</MenuItem>
+                                    <MenuItem onClick={handleClose}>Elemen Editing</MenuItem>
+                                </Menu>
+                            </>
+                        ) : (
+                            <>
+                                <Breadcrumbs aria-label="breadcrumb">
+                                    <Link underline="hover" color="inherit" href="/">
+                                        Home
+                                    </Link>
+                                    <Link
+                                        underline="hover"
+                                        color="inherit"
+                                        href="/about"
+                                    >
+                                        Practice Selection
+                                    </Link>
+                                </Breadcrumbs>
+                            </>
+                        )}
+
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}
+
+export default Header;

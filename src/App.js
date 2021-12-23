4e0b5d31ab1e1584ddcd389a990ba3f749c3bca3
mcelhinneyk <mr.mcelhinney@gmail.com>
2021-12-23 14:50:47 -0800

    added client side routing to App.jsusing react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import Header from './Header';
-// import PracticeSelection from './PracticeSelection';
-// import PracticeSettings from './PracticeSettings';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import PracticeSelection from './PracticeSelection';
+import PracticeSettings from './PracticeSettings';
 import QuizRender from './QuizRender';
-// import HighScores from './HighScores';
-import { CssBaseline, Container } from '@material-ui/core';
+import HighScores from './HighScores';
+import { Container } from '@material-ui/core';
 
 
 
@@ -12,26 +12,34 @@ function App() {
   const [questionSettings, setQuestionSettings] = useState({
     numberOfQuestions: 5,
     numberOfElements: 2,
-      questionType:"predictBond",
-      questionStem: "Predict if {name} and {name} will produce an ionic, polar covalent, or non-polar covalent bond?",
-      answerChoices: [
-        {answerText: "ionic", isCorrect:false},
-        {answerText:"polar covalent", isCorrect:false},
-        {answerText:"non-polar covalent", isCorrect:false}
-      ]
+    questionType: "predictBond",
+    questionStem: "Predict if {name} and {name} will produce an ionic, polar covalent, or non-polar covalent bond?",
+    answerChoices: [
+      { answerText: "ionic", isCorrect: false },
+      { answerText: "polar covalent", isCorrect: false },
+      { answerText: "non-polar covalent", isCorrect: false }
+    ]
   })
   return (
     <>
-      <CssBaseline />
-      <Header />
-      <div>
+      <BrowserRouter>
         <Container>
-          {/* <PracticeSelection /> */}
-          {/* <PracticeSettings /> */}
-          <QuizRender questionSettings={questionSettings}/>
-          {/* <HighScores /> */}
+          <Switch>
+            <Route path="/PracticeSettings">
+              <PracticeSettings />
+            </Route>
+            <Route path="/Quiz">
+              <QuizRender questionSettings={questionSettings} />
+            </Route>
+            <Route path="/HighScore">
+              <HighScores />
+            </Route>
+            <Route exact path="/">
+              <PracticeSelection />
+            </Route>
+          </Switch>
         </Container>
-      </div>
+      </BrowserRouter>
 
     </>
   );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import Header from './Header';
 import App from './App';
+import { CssBaseline, Container } from '@material-ui/core';
+
+
 
 ReactDOM.render(
-  <>
+  <Container>
+    <CssBaseline />
+    <Header />
     <App />
-  </>,
+  </Container>,
   document.getElementById('root')
 );

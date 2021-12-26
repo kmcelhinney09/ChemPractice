48ce706d335703a2e53b7a5e3e030a5b7688e792
mcelhinneyk <mr.mcelhinney@gmail.com>
2021-12-26 14:45:22 -0800

    added a controled field for highs scorer initals and a submit button wiht handeling

--- src/ScorePage.js
+++ src/ScorePage.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { Card, Container, CardContent, Button } from "@mui/material";
+import { Card, Container, CardContent, Button, Typography, TextField, Grid } from "@mui/material";
 import { useHistory } from "react-router-dom";
 
 function ScorePage({ score, total, highScoreData, questionType }) {
     const history = useHistory();
     let highScoreID = 0;
-    let questionCatagory; 
+    let questionCatagory;
+    let questionCatagoryID = 0;
+    const [highScoreUpdate, setHighScoreUpdate] = useState(false)
+    const [highScoreInitals, setHighScoreInitals] = useState('')
 
     useEffect(() => {
         highScoreQualification()
@@ -14,41 +17,72 @@ function ScorePage({ score, total, highScoreData, questionType }) {
     function highScoreQualification() {
         if (questionType === 'metalNonMetal') {
             questionCatagory = 'Bond Type'
+            questionCatagoryID = 1
         } else if (questionType === 'predictBond') {
             questionCatagory = 'Bond Prediction'
+            questionCatagoryID = 2
         } else if (questionType === 'electronegativityDifference') {
             questionCatagory = 'Electronegativity Difference'
+            questionCatagoryID = 3
         }
+
         highScoreData.forEach(highScores => {
             if (highScores.category === questionCatagory) {
                 highScores.scores.every(scoreData => {
-                    if(scoreData.score < score){
+                    if (scoreData.score < score) {
                         highScoreID = scoreData.id
+                        setHighScoreUpdate(true)
                         return false
-                    }else{
+                    } else {
                         return true
                     }
                 })
             }
         })
-        console.log(highScoreID)
     }
 
     function handleClick() {
         history.push("/HighScore")
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(highScoreInitals)
+    }
+
     return (
         <Container>
             <Card>
                 <CardContent>
-                    <h1>Your score is {score} out of {total}</h1>
-                    <h1>That is {(score / total) * 100}%</h1>
+                    <Typography variant="h4">Your score is {score} out of {total}</Typography>
+                    <Typography variant="h4">That is {(score / total) * 100}%</Typography>
                 </CardContent>
-                <Button variant="contained" onClick={handleClick}>High Scores</Button>
             </Card>
-        </Container>
+            {highScoreUpdate ? <Card>
+                <CardContent>
+                    <Typography variant="h4">Congradulations You have made the High Score Roster!</Typography>
+                    <form autoComplete="off" onSubmit={handleSubmit}>
+                        <Grid container alignItems="left" direction="column">
+                            <Grid item>
+                                <TextField
+                                    variant="outlined"
+                                    label="Initals"
+                                    required
+                                    defaultValue={highScoreInitals}
+                                    onChange={(e) => setHighScoreInitals(e.target.value)}
+                                />
+                            </Grid>
+                            <Grid item>
+                                <Button varient="contained" type="submit">Submit Initals</Button>
+                            </Grid>
+                        </Grid>
 
+
+                    </form>
+                </CardContent>
+            </Card> : null}
+            <Button variant="contained" onClick={handleClick}>High Scores</Button>
+        </Container>
     )
 }
 
